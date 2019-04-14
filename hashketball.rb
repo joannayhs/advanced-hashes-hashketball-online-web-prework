d32447904340c93f86532a81346a08d617ff7397
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-14 00:32:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -227,7 +227,11 @@ def winning_team
       end
     end
   end
-  charlotte_array.collect{|x| x+x}
-  brooklyn_array.collect{|x| x+x}
-  binding.pry
+  if charlotte_array.sum > brooklyn_array.sum
+   game_hash.each do |location, team_data|
+     if location == :away
+       return team_data[:team_name]
+     end
+     elsif charlotte_array.sum < brooklyn_array.sum
+     return team_data[:team_name]
 end
